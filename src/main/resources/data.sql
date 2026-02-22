fae035590be5a61ffff694d2b75158fbb55079ea
JhonCooktoday <jhonramos3456@gmail.com>
2026-02-22 15:38:52 -0500

    BD

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,15 @@
-INSERT INTO especie (definicion, imagen, nombre_especie)
+INSERT INTO especie (id_especie, definicion, imagen, nombre_especie)
 VALUES (
+'11111111-1111-1111-1111-111111111111',
 'Animal Domestico',
 'https://jpgiptacggdxrusfhbxd.supabase.co/storage/v1/object/public/razas/cuy.jpg',
 'Cuy'
 );
-INSERT INTO especie (definicion, imagen, nombre_especie)
+
+INSERT INTO especie (id_especie, definicion, imagen, nombre_especie)
 VALUES (
+'22222222-2222-2222-2222-222222222222',
 'Animal Domestico',
 'https://jpgiptacggdxrusfhbxd.supabase.co/storage/v1/object/public/razas/gato.jpg',
 'Gato'
 );
-
-
